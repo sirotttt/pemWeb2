97a67df9773fad3249fb6f040b79413f4fa9a347
sirotttt <116140114+sirotttt@users.noreply.github.com>
2023-03-16 15:28:45 +0700

    praktikum4

---
+++ praktikum4/function.php
@@ -0,0 +1,12 @@
+<?php
+include "rumus.php";
+
+luasLingkaran(15);
+
+echo "<br>";
+
+luasPersegi(30);
+
+echo "<br>";
+
+luasSegitiga(13, 40);
---
+++ praktikum4/project/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php require_once "uji/head.php" ?>
+    </head>
+    <body class="sb-nav-fixed">
+        <!-- navbar -->
+        <?php include_once "uji/navbar.php" ?>
+        <!-- sidebar -->
+        <?php include "uji/sidebar.php" ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+</html>
